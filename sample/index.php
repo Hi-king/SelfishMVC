5429d719ccc96e19d5ccb2c0ec296db3dfd3725a
Keisuke OGAKI <keisuke_ogaki@dwango.co.jp>
2013-11-19 20:22:36 +0900

    refactor

--- sample/index.php
+++ sample/index.php
@@ -1,32 +1,35 @@
 <?php
+/*
+  Application definition class
+ */
 require_once '../Application.php';
 
 class RSSApplication extends Application {
   protected function route() {
-     require_once dirname(__FILE__).'/controllers/RSSController.php'; /* TODO: Namespace... */
+    // load controllers
+    require_once dirname(__FILE__).'/controllers/RSSController.php'; /* TODO: Namespace... */
+     
+    // routing definition
     return array(
       '/hello/' => array(
-           'controller' => 'RSSController', 
-           'action' => 'hello'
+        'controller' => 'RSSController', 
+        'action' => 'hello'
       ),
       '/top/' => array(
-          'controller' => 'RSSController', 
-          'action' => 'top'
-      ), 
-      '/param/:id/' => array(
-          'controller' => 'RSSController', 
-          'action' => 'param', 
-          'argnames' => array('id')
+        'controller' => 'RSSController', 
+        'action' => 'top'
       ),
-   );
+      '/param/:id/' => array(
+        'controller' => 'RSSController', 
+        'action' => 'param', 
+        'argnames' => array('id')
+      ),
+    );
   }
 }
 
+
+// start Applitcation
 (new RSSApplication())->run($_SERVER);
 
-
-/* require 'core/Request.php'; */
-/* require 'core/Router.php'; */
-/* $req = new Request($_SERVER); */
-/* Router::resolve($req); */
 
