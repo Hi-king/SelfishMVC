2f879d46ec0cb20e19254bae764a5838670380a4
Keisuke OGAKI <keisuke_ogaki@dwango.co.jp>
2013-11-25 13:55:30 +0900

    use smarty template

---
+++ core/CoreBootLoader.php
@@ -0,0 +1,7 @@
+<?php
+// core
+require_once 'Request.php';
+require_once 'Router.php';
+
+// smarty
+require_once '/usr/local/lib/Smarty-3.1.15/libs/Smarty.class.php';
--- sample/controllers/RSSController.php
+++ sample/controllers/RSSController.php
@@ -1,7 +1,19 @@
 <?php
 class RSSController{
   function hello() {
-    echo "<p>Hello, World</p>";
+    $smarty = new Smarty();
+    $smarty->template_dir = dirname(__FILE__).'/../view/templates/';
+    $smarty->compile_dir  = dirname(__FILE__).'/../view/templates_c/';
+    $smarty->config_dir   = dirname(__FILE__).'/../view/config/';
+    $smarty->cache_dir    = dirname(__FILE__).'/../view/cache/';
+
+
+    $smarty->assign('name', 'Anonymous');
+    $smarty->assign('contents', 
+      $smarty->fetch('hello.tpl')
+    );
+    $smarty->display('layout.tpl');
+    //echo "<p>Hello, World</p>";
   }
   function top() {
     echo "<p>Top page?</p>";
