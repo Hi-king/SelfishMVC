4028b8c8f44ee1de1c62356288accc090ebd484a
Keisuke OGAKI <keisuke_ogaki@dwango.co.jp>
2013-12-16 16:23:01 +0900

    remove no longer used files

--- sample/models/User.php
+++
@@ -1,9 +0,0 @@
-<?php
-class User{
-  private static $VALID_USER = array('anonymous', 'hiking');
-  public static function login($username) {
-    if(!in_array($username, self::$VALID_USER)) {
-      throw new Exception($username." is not a valid user.");
-    }
-  }
-}
--- sample/view/templates/top.tpl
+++
@@ -1,7 +0,0 @@
-<h1>RSS Reader(β)</h1>
-<p>
-  RSS2.0 URL: 
-  <form action="../show/" method="GET">
-    <input type=text name="url" value="http://d.hatena.ne.jp/Chikirin/rss2" />
-  </form>
-</p>
