3e9199d973c3e5c13b837785a8b85b31b51f11ce
Keisuke OGAKI <keisuke_ogaki@dwango.co.jp>
2014-04-07 16:47:53 +0900

    fix test

--- test/core/ControllerTest.php
+++ test/core/ControllerTest.php
@@ -1,13 +1,29 @@
 <?php
 require_once '/usr/local/lib/Smarty-3.1.15/libs/Smarty.class.php';
+require_once __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__.'/../../Application.php';
 require_once dirname(__FILE__).'/../../core/Controller.php';
+
+class TestApplication extends Application {
+  public function get_doctrine() {
+    $isDevMode = true;
+    $config = \Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration(array(__DIR__."/models"), $isDevMode);
+    $conn = array(
+                  'driver' => 'pdo_sqlite',
+                  'memory' => true
+                  );
+    return \Doctrine\ORM\EntityManager::create($conn, $config);
+  }
+}
 
 class ControllerTest extends PHPUnit_Framework_TestCase
 {
   
   function setup() {
+    $app = new TestApplication();
+    $em = $app->get_doctrine();
     $dummy_smarty = new Smarty();
-    $this->controller = new Controller($dummy_smarty);
+    $this->controller = new Controller($dummy_smarty, $em);
   }
 
   /**
